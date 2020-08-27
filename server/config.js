68fa42a1dc635861629fc6d4d1d1b64b6c0d1074
maulik kanani <mk.globalia@gmail.com>
2020-08-27 11:47:18 +0530

    first commit

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    '+srv://imongodbnvoiceSystem:9s1asbk9weCocMyg@pm.1tahh.mongodb.net/graphql?retryWrites=true&w=majority',
+  SECRET_KEY: 'some very secret key'
+};
---
+++ server/models/User.js
@@ -0,0 +1,11 @@
+const { model, Schema } = require('mongoose');
+
+const userSchema = new Schema({
+  username: String,
+  password: String,
+  email: String,
+  createdAt: String,
+  updatedAt : String
+});
+
+module.exports = model('User', userSchema);
